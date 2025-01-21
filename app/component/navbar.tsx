cc76763b13b580c8efced32919eadd755dbf8778
InwSaKodBeiw007 <gamekung2707@gmail.com>
2025-01-21 11:17:11 +0700

    Hot-Fixs

---
+++ app/component/navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <div>
+            <ul>
+                <li><Link href="../">HOME-Pages</Link></li>
+                <li><Link href="\page1">Go to First-Pages </Link></li>
+                <li><Link href="\page2">Go to Second-Pages</Link></li>
+                <li><Link href="\page3">Go to Third-Pages</Link></li>
+            </ul>
+        </div>
+    );
+}
