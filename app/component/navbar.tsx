3f09559edbf9922d7e247b6c3e6d0d85ad16d987
InwSaKodBeiw007 <gamekung2707@gmail.com>
2025-01-21 11:22:26 +0700

    Fixsing

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -8,6 +8,7 @@ export default function Page() {
                 <li><Link href="\page1">Go to First-Pages </Link></li>
                 <li><Link href="\page2">Go to Second-Pages</Link></li>
                 <li><Link href="\page3">Go to Third-Pages</Link></li>
+                <li><Link href="\Counter">Go to counter</Link></li>
             </ul>
         </div>
     );
